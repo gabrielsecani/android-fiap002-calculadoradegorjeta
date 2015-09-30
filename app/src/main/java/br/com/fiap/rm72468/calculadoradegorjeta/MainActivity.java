06faaec89d47ded40ce2778d32f349feb5a871fe
rm72468 <gabrielltr84@gmail.com>
2015-09-29 21:05:19 -0300

    Adicionados eventos para calcular porcentagens.

--- app/src/main/java/br/com/fiap/rm72468/calculadoradegorjeta/MainActivity.java
+++ app/src/main/java/br/com/fiap/rm72468/calculadoradegorjeta/MainActivity.java
@@ -2,8 +2,11 @@ package br.com.fiap.rm72468.calculadoradegorjeta;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
@@ -14,28 +17,67 @@ public class MainActivity extends ActionBarActivity {
 
     //formatadores
     NumberFormat formatadorDePorcentagem = NumberFormat.getPercentInstance();
-    //NumberFormat formatadorDeMoeda = NumberFormat.getCurrencyInstance();
+    NumberFormat formatadorDeMoeda = NumberFormat.getCurrencyInstance();
 
     //variaveis de componente de tela
     private SeekBar seekBar;
     private TextView lblPorcentagemVariavel;
+    private TextView lblPorcentagemPadrao;
 
-    private double porcentagemVariavel;
+    private double porcentagemVariavel = 0.18;
+    private double porcentagemPadrao = 0.1;
+    private double valor = 0.0;
+
+
+    private EditText txtValor;
+    private TextView lblGorjetaVariavel;
+    private TextView lblTotalPagarGorjetaVariavel;
+    private TextView lblGorjetaPadrao;
+    private TextView lblTotalPagarGorjetaPadrao;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        lblPorcentagemPadrao = (TextView)findViewById(R.id.lblPorcentagemPadrao);
         lblPorcentagemVariavel = (TextView)findViewById(R.id.lblPorcentagemVariavel);
-        porcentagemVariavel = 18;
-        seekBar = (SeekBar)findViewById(R.id.seekBar);
+        lblGorjetaVariavel = (TextView)findViewById(R.id.lblGorjetaVariavel);
+        lblTotalPagarGorjetaVariavel = (TextView)findViewById(R.id.lblTotalPagarGorjetaVariavel);
+        lblGorjetaPadrao = (TextView)findViewById(R.id.lblGorjetaPadrao);
+        lblTotalPagarGorjetaPadrao = (TextView)findViewById(R.id.lblTotalPagarGorjetaPadrao);
+        txtValor = (EditText)findViewById(R.id.txtValor);
+        txtValor.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                try {
+                    valor = Double.parseDouble(s.toString());
+                }catch (NumberFormatException e){
+                    valor = 0.0;
+                }
+                atualizarPorcentagemVariavel();
+                atualizarPorcentagemPadrao();
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+        lblPorcentagemPadrao.setText(formatadorDePorcentagem.format(porcentagemPadrao));
+
+        seekBar = (SeekBar)findViewById(R.id.seekBar);
         seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
                 porcentagemVariavel = progress/100.0;
                 lblPorcentagemVariavel.setText(formatadorDePorcentagem.format(porcentagemVariavel));
+                atualizarPorcentagemVariavel();
             }
 
             @Override
@@ -50,6 +92,18 @@ public class MainActivity extends ActionBarActivity {
         });
     }
 
+    private void atualizarPorcentagemVariavel() {
+        double gorjetaVariavel = valor * porcentagemVariavel;
+        double total = valor + gorjetaVariavel;
+        lblGorjetaVariavel.setText(formatadorDeMoeda.format(gorjetaVariavel));
+        lblTotalPagarGorjetaVariavel.setText(formatadorDeMoeda.format(total));
+    }
+    private void atualizarPorcentagemPadrao() {
+        double gorjetaPadrao = valor * porcentagemPadrao;
+        double total = valor + gorjetaPadrao;
+        lblGorjetaPadrao.setText(formatadorDeMoeda.format(gorjetaPadrao));
+        lblTotalPagarGorjetaPadrao.setText(formatadorDeMoeda.format(total));
+    }
 
 
     @Override
